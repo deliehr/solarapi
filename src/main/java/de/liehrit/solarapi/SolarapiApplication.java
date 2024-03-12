475bca21b99e865335e360d130dc244f0057f863
Dominik Liehr <dominik_liehr@me.com>
2024-03-12 22:23:40 +0100

    Initial commit

---
+++ src/main/java/de/liehrit/solarapi/SolarapiApplication.java
@@ -0,0 +1,13 @@
+package de.liehrit.solarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/liehrit/solarapi/SolarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.liehrit.solarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
